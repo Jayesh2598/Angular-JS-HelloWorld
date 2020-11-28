db1cb6436e3257af62ec980fe7fdc0103f29cf07
Jay002 <jayesh2598@gmail.com>
2020-11-28 23:29:49 +0530

    Added: Regex validation of username entered by user.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ export class AppComponent {
   imgUrl;
   url;
   userName: string = "";
+  nameError: string = "";
 
   ngOnInit(): void {
     this.title = 'Hello from Bridgelabz';
@@ -20,5 +21,13 @@ export class AppComponent {
   onClick($event) {
     console.log("Save button is clicked!", $event);
     window.open(this.url);
+  }
+
+  onInput($event) {
+    const nameRegex = RegExp("^[A-Z]{1}[A-Za-z\\s]{2,}$");
+    if(!(nameRegex.test(this.userName)))
+      this.nameError = "Invalid name";
+    else
+      this.nameError = "";
   }
 }
