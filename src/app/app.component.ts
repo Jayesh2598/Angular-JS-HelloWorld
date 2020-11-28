2d2628cbc0a878ea21ebbf0c9ec8794b457544f2
Jay002 <jayesh2598@gmail.com>
2020-11-28 20:59:23 +0530

    Added: Code to launch site in new tab on clicking logo using event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,16 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'HelloWorld';
   imgUrl;
+  url;
 
   ngOnInit(): void {
     this.title = 'Hello from Bridgelabz';
     this.imgUrl = '../assets/BL_logo_square_jpg.jpg';
+    this.url = 'https://www.bridgelabz.com';
+  }
+
+  onClick($event) {
+    console.log("Save button is clicked!", $event);
+    window.open(this.url);
   }
 }
